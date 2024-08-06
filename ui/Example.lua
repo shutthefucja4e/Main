2352095a626158d85fd189b4daeb437e0d39c989
shutthefucja4e <160066816+shutthefucja4e@users.noreply.github.com>
2024-08-07 01:41:31 +0500

    Update Example.lua

--- ui/Example.lua
+++ ui/Example.lua
@@ -1,3 +1,5 @@
+-- https://github.com/dawid-scripts/Fluent
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
