8e3e6c649455fc15bef0384585239694783c9f0a
shutthefucja4e <160066816+shutthefucja4e@users.noreply.github.com>
2024-08-07 23:16:42 +0500

    Update TtTD.lua

--- Supported/TtTD.lua
+++ Supported/TtTD.lua
@@ -8,7 +8,7 @@ local Window = Fluent:CreateWindow({
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true,
-    Theme = "Rose",
+    Theme = "Dark",
     MinimizeKey = Enum.KeyCode.LeftControl
 })
 
